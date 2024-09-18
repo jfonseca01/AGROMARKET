42c464b9fe9d8a9ac9f7dcc3948ce5e903204742
fonseca01 <jefferson.fonseca994@gmail.com>
2024-09-17 21:24:44 -0500

    Se agrega el proyecto

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Se ejecuta prueba de JDK Y CONEXION CON GITHUB
+        System.out.println("Prueba conexion github");
+    }
+}
